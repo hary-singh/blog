4046eff2478f03e2a444155e593addb042361dd5
Harry Dhillon <navbirsdhillon@gmail.com>
2021-03-01 21:10:54 -0700

    page delete function added

--- app/javascript/components/Pages.js
+++ app/javascript/components/Pages.js
@@ -1,21 +1,21 @@
 import React from 'react';
-
-const url = "http://localhost:3000/pages/"
-
-const Pages = ({pages}) => (
-    <>
-        <h1 className='header'> All Pages</h1>
-        <a href={url + "new"}> New Page</a>
-        <ul> 
-            { pages.map( p=> (
-                <li key ={p.id}>
-                    {p.title}
-                    <a href={ url + p.id}> show </a>
-                </li>
-            ))}
-
-        </ul>
-    </>
+const url = "http://localhost:3000/pages/";
+const Pages = ({ pages }) => (
+  <>
+    <h1 className='header'>All Pages</h1>
+    <a href={url + "new"}>New pages</a>
+    <ul>
+      { pages.map( p => (
+        <li key={p.id}>
+          {p.title}
+          <a href={ url + p.id }>
+            show
+          </a>
+          <a href={ url + p.id + '/edit'}>edit</a>
+          <a href={ url + p.id} data-method="delete">delete</a>
+        </li>
+      ))}
+    </ul>
+  </>
 )
-
 export default Pages;
